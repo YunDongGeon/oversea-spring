7ff223145575d9463f94be969bbebc96908fd3b9
JH Park <pjh0410@gmail.com>
2019-06-08 11:08:25 +0900

    m

--- src/main/java/itc/hoseo/oversea/destination/Destination.java
+++ src/main/java/itc/hoseo/oversea/destination/Destination.java
@@ -1,26 +1,16 @@
 package itc.hoseo.oversea.destination;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Destination {
 	private String destiName;
 	private String destiKorName;
 	private float destiMoney;
 	
-	public float getDestiMoney() {
-		return destiMoney;
-	}
-	public void setDestiMoney(float destiMoney) {
-		this.destiMoney = destiMoney;
-	}
-	public String getDestiName() {
-		return destiName;
-	}
-	public void setDestiName(String destiName) {
-		this.destiName = destiName;
-	}
-	public String getDestiKorName() {
-		return destiKorName;
-	}
-	public void setDestiKorName(String destiKorName) {
-		this.destiKorName = destiKorName;
-	}
 }
--- src/main/java/itc/hoseo/oversea/nav/NavController.java
+++ src/main/java/itc/hoseo/oversea/nav/NavController.java
@@ -1,7 +1,16 @@
 package itc.hoseo.oversea.nav;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.google.common.collect.ImmutableList;
+
+import itc.hoseo.oversea.destination.Destination;
 
 @Controller
 public class NavController {
@@ -9,8 +18,27 @@ public class NavController {
 	public String index() {
 		return "index";
 	}
+	
+	
+	
 	@GetMapping("/overseaDestiList")
-	public String destiList() {
+	public String destiList(ModelMap m, Destination dest) {
+		List<Destination> destList = ImmutableList.of(
+				new Destination("Hong Kong","홍콩",100.0f), 
+				new Destination("Bang Kook","방콕",200.0f)); 
+		
+		m.put("destList" , destList);
+		
+		if(dest != null && StringUtils.isEmpty(dest.getDestiName()) == false) {
+			m.put("destList", destList.stream()
+				.filter(d -> {
+					return d.getDestiKorName().contains(dest.getDestiName()) ||
+							d.getDestiName().contains(dest.getDestiName());
+				}).collect(Collectors.toList()));
+		}
+		
+		
+		
 		return "shop/overseaDestiList";
 	}
 	@GetMapping("/overseaSupport")
